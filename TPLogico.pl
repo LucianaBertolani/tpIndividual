23de00e9d5c825a0715d1f3d967ac50fd0f81400
LucianaBertolani <lbertolani@frba.utn.edu.ar>
2021-08-11 21:07:06 -0300

    Correccion issues: menos hechos, mas reglas.

--- TPLogico.pl
+++ TPLogico.pl
@@ -4,39 +4,50 @@ viveEn(mansionDreadbury,agatha).
 viveEn(mansionDreadbury,mayordomo).
 viveEn(mansionDreadbury,charles).
 
-odiaA(mayordomo,charles).
-odiaA(charles,agatha).
-odiaA(agatha,agatha).
-odiaA(charles,mayordomo).
-odiaA(agatha,mayordomo).
+odiaA(Odiado,agatha):-
+  viveEn(mansionDreadbury,Odiado),
+  Odiado \= mayordomo.
 
-esMasRico(mayordomo,agatha).
+odiaA(Odiado,charles):-
+  viveEn(mansionDreadbury,Odiado),
+  not(odiaA(Odiado,agatha)).
+
+odiaA(Odiado,mayordomo):-
+  odiaA(Odiado,agatha).
+
+esMasRico(Rico,agatha):-
+  viveEn(mansionDreadbury,Rico),
+  not(odiaA(Rico,mayordomo)).
 
 mataA(Victima,Asesino):-
+  viveEn(mansionDreadbury,Asesino),
   odiaA(Victima,Asesino),
-  esMasRico(Victima,Asesino),
-  viveEn(mansionDreadbury,Asesino).
+  not(esMasRico(Asesino,Victima)).
 
-/*
-1)  ?- mataA(agatha,Quien).
-    false.
-2)  ?- odiaA(milhouse,_).
-    false.
-    ?- odiaA(Quien,charles).
-    mayordomo.
-    ?- odiaA(agatha,Quien).
-    charles;
-    agatha.
-    ?- odiaA(Odiado,Odiador).
-    Odiado = mayordomo,
-    Odiador = charles ;
-    Odiado = charles,
-    Odiador = agatha ;
-    Odiado = Odiador, Odiador = agatha ;
-    Odiado = charles,
-    Odiador = mayordomo ;
-    Odiado = agatha,
-    Odiador = mayordomo.
-    ?- odiaA(_,mayordomo).
-    true.
+/* 
+1 ?- mataA(agatha,Quien).
+Quien = agatha ;
+false.
+
+2 ?- odiaA(milhouse,_).
+false.
+
+3 ?- odiaA(Quien,charles).
+Quien = mayordomo ;
+false.
+
+4 ?- odiaA(agatha,Quien).
+Quien = agatha ;
+Quien = mayordomo.
+
+5 ?- odiaA(Odiado,Odiador).
+Odiado = Odiador, Odiador = agatha ;
+Odiado = charles, Odiador = agatha ;
+Odiado = mayordomo, Odiador = charles ;
+Odiado = agatha, Odiador = mayordomo ;
+Odiado = charles, Odiador = mayordomo.
+
+6 ?- odiaA(_,mayordomo).
+true ;
+true.
 */
